f8ffa0d3e6305cd2a534e70e7d2b3eb521886118
Aihasas Yadu <yadu.aihasas@gmail.com>
2026-04-09 03:19:13 +0530

    -->Gameplay Abilities plugin and dependency modules added

--- Source/AbilitySystem/AbilitySystem.Build.cs
+++ Source/AbilitySystem/AbilitySystem.Build.cs
@@ -8,6 +8,6 @@ public class AbilitySystem : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "GameplayAbilities", "GameplayTags", "GameplayTasks", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
     }
 }
