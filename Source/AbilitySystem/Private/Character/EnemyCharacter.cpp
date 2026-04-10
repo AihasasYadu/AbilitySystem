b9762464c41b302de20936cf11bd0957e21cbf96
Aihasas Yadu <yadu.aihasas@gmail.com>
2026-04-10 17:42:27 +0530

    -->Enemy Character Class added

---
+++ Source/AbilitySystem/Private/Character/EnemyCharacter.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/EnemyCharacter.h"
+
+
+// Sets default values
+AEnemyCharacter::AEnemyCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UGameAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+	AttributeSet = CreateDefaultSubobject<UAttributeSetBase>("AttributeSet");
+}
+
+// Called when the game starts or when spawned
+void AEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+	GiveDefaultAbilities();
+	InitDefaultAttributes();
+}
+
