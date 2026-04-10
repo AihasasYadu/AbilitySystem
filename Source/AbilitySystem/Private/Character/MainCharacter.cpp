eba490f49fcda388f464ea70b399d4cbd4e76e29
Aihasas Yadu <yadu.aihasas@gmail.com>
2026-04-10 17:42:03 +0530

    -->Main Character class added

---
+++ Source/AbilitySystem/Private/Character/MainCharacter.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/MainCharacter.h"
+
+#include "Player/GamePlayerState.h"
+
+
+// Sets default values
+AMainCharacter::AMainCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AMainCharacter::InitAbilitySystemComponent()
+{
+	AGamePlayerState* CurrentPlayerState = GetPlayerState<AGamePlayerState>();
+	check(CurrentPlayerState);
+	AbilitySystemComponent =  CastChecked<UGameAbilitySystemComponent>(CurrentPlayerState->GetAbilitySystemComponent());
+	AbilitySystemComponent->InitAbilityActorInfo(CurrentPlayerState, this);
+	AttributeSet = CurrentPlayerState->GetAttributeSet();
+}
+
+// Called when the game starts or when spawned
+void AMainCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMainCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void AMainCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+	InitAbilitySystemComponent();
+	GiveDefaultAbilities();
+	InitDefaultAttributes();
+}
+
+void AMainCharacter::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+	InitAbilitySystemComponent();
+	InitDefaultAttributes();
+}
---
+++ Source/AbilitySystem/Public/Character/MainCharacter.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharacterBase.h"
+#include "MainCharacter.generated.h"
+
+UCLASS()
+class ABILITYSYSTEM_API AMainCharacter : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+private:
+	void InitAbilitySystemComponent();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Sets default values for this character's properties
+	AMainCharacter();
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_PlayerState() override;
+};
