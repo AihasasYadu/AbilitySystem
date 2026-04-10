581da22b49a5b592ac0d12dd232498ab7c0ca109
Aihasas Yadu <yadu.aihasas@gmail.com>
2026-04-10 17:44:24 +0530

    -->Custom Player State script added

---
+++ Source/AbilitySystem/Private/Player/GamePlayerState.cpp
@@ -0,0 +1,24 @@
+﻿#include "Player/GamePlayerState.h"
+
+#include "AbilitySystemComponent.h"
+#include "Abilities/AttributeSetBase.h"
+#include "Abilities/GameAbilitySystemComponent.h"
+
+AGamePlayerState::AGamePlayerState()
+{
+	SetNetUpdateFrequency(100.0f);
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UGameAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+	AttributeSet = CreateDefaultSubobject<UAttributeSetBase>("AttributeSet");
+}
+
+UAbilitySystemComponent* AGamePlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+UAttributeSetBase* AGamePlayerState::GetAttributeSet() const
+{
+	return AttributeSet;
+}
---
+++ Source/AbilitySystem/Public/Player/GamePlayerState.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/PlayerState.h"
+#include "GamePlayerState.generated.h"
+
+class UGameAbilitySystemComponent;
+class UAttributeSetBase;
+
+UCLASS()
+class ABILITYSYSTEM_API AGamePlayerState : public APlayerState, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+protected:	
+	UPROPERTY()
+	TObjectPtr<UGameAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY()
+	TObjectPtr<UAttributeSetBase> AttributeSet;
+	
+public:
+	AGamePlayerState();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	virtual UAttributeSetBase* GetAttributeSet() const;
+};
